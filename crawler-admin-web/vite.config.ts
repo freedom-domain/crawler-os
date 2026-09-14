3691d29c146f36a143f84a26c9675393af7b0cc2
leo wu <leo.wu.email@gmail.com>
2026-09-14 20:42:55 +0800

    update

--- crawler-admin-web/vite.config.ts
+++ crawler-admin-web/vite.config.ts
@@ -10,7 +10,9 @@ export default defineConfig({
     }
   },
   server: {
-    port: 5173,
+    host: true,
+    port: 58080,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8080',
