44181c8c25848e768e3e9a92b8c9c59fb249e2e4
leo wu <leo.wu.email@gmail.com>
2026-09-14 19:55:51 +0800

    update

--- crawler-common/src/main/java/com/collect/common/entity/BaseEntity.java
+++ crawler-common/src/main/java/com/collect/common/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
